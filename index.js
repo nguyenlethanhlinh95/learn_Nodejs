f2a2869a9dccb544decba8e4ccc583f1c9b90a40
Thanh Linh <nguyenlethanhlinh95@gmail.com>
2019-05-10 11:17:41 +0700

    Hello word express js

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.get('/', function (request, response) { 
+    response.send('<h1>Hello coders.Tokyo</h1>');
+ });
+
+
+ app.listen(port, function(){
+    console.log('Server listening on port ' + port);
+ });
